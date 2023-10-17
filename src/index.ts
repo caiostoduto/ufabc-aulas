a747e0d7016402a0d2a36561ab91d00e5ec25e23
Caio Stoduto <caio.stoduto@outlook.com>
2023-10-17 18:43:12 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+module.exports = class UFABCAulas {
+  urlCalendarios = 'https://prograd.ufabc.edu.br/calendarios'
+  urlMatriculas = 'https://prograd.ufabc.edu.br/matriculas'
+
+  async fetch (): Promise<UFABCAulas> {
+    return this
+  }
+}
