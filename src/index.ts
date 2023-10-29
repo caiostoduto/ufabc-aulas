759197296c97130aa92e59d787280a656a0e0bb4
Caio Stoduto <caio.stoduto@outlook.com>
2023-10-29 19:23:42 -0300

    feat: fetch & parse current year's UFABC Quadrimestres

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
+import { fetchCalendarioAtualURL, fetchQuadris, type quadri } from './quadris'
+
 module.exports = class UFABCAulas {
-  urlCalendarios = 'https://prograd.ufabc.edu.br/calendarios'
-  urlMatriculas = 'https://prograd.ufabc.edu.br/matriculas'
+  CALENDARIOS_URL = new URL('https://prograd.ufabc.edu.br/calendarios')
+  MATRICULAS_URL = new URL('https://prograd.ufabc.edu.br/matriculas')
+
+  calendarioAtualURL: URL | undefined
+  quadris: quadri[] = []
 
   async fetch (): Promise<UFABCAulas> {
+    await this.fetchQuadris()
+
+    return this
+  }
+
+  async fetchQuadris (): Promise<UFABCAulas> {
+    this.calendarioAtualURL = await fetchCalendarioAtualURL(this.CALENDARIOS_URL)
+    this.quadris = await fetchQuadris(this.calendarioAtualURL)
+
     return this
   }
 }
