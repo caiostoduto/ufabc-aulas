6d35beff6b51ce298206cd4a0e087e9925fbc875
Caio Stoduto <caio.stoduto@outlook.com>
2023-10-29 21:45:54 -0300

    feat: fetch & parse current year's UFABC student matriculas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { fetchMatriculasAtualURL, fetchMatriculas } from './matriculas'
 import { fetchCalendarioAtualURL, fetchQuadris, type quadri } from './quadris'
 
 module.exports = class UFABCAulas {
@@ -5,10 +6,14 @@ module.exports = class UFABCAulas {
   MATRICULAS_URL = new URL('https://prograd.ufabc.edu.br/matriculas')
 
   calendarioAtualURL: URL | undefined
-  quadris: quadri[] = []
+  matriculasAtualURL: URL | undefined
+  quadris: quadri[] | undefined
+  matriculas: Map<string, string[]> | undefined
+  turmas: Map<string, string[]> | undefined
 
   async fetch (): Promise<UFABCAulas> {
     await this.fetchQuadris()
+    await this.fetchMatriculas()
 
     return this
   }
@@ -16,6 +21,13 @@ module.exports = class UFABCAulas {
   async fetchQuadris (): Promise<UFABCAulas> {
     this.calendarioAtualURL = await fetchCalendarioAtualURL(this.CALENDARIOS_URL)
     this.quadris = await fetchQuadris(this.calendarioAtualURL)
+
+    return this
+  }
+
+  async fetchMatriculas (): Promise<UFABCAulas> {
+    this.matriculasAtualURL = await fetchMatriculasAtualURL(this.MATRICULAS_URL)
+    this.matriculas = await fetchMatriculas(this.matriculasAtualURL)
 
     return this
   }
