15db5b624c8ccd200fc233d7d50ccb3aeba52852
Caio Stoduto <caio.stoduto@outlook.com>
2023-11-15 20:10:57 -0300

    refactor: improving code quality for future features

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import axios, { type AxiosRequestConfig } from 'axios'
 import { getDocument } from 'pdfjs-dist'
 import { type DocumentInitParameters, type TextItem } from 'pdfjs-dist/types/src/display/api'
+import { type CheerioAPI, load } from 'cheerio'
 
 export async function fetch (url: string, config: AxiosRequestConfig = {}): Promise<any> {
   const res = await axios.get(url, config)
@@ -21,4 +22,28 @@ export async function extractTextsFromPdf (docParams: DocumentInitParameters): P
   }
 
   return txts
+}
+
+export async function findHref (url: URL, contains: string): Promise<[CheerioAPI, URL]> {
+  const html = await fetch(url.toString())
+  const $ = load(html)
+  const elements = $(`a:contains("${contains}")`)
+  if (elements.length === 0) {
+    throw new Error()
+  } else {
+    return [$, new URL(`https://${url.host}${elements.attr('href') as string}`)]
+  }
+}
+
+export async function fetch$Matriculas (atual: URL, anteriores: URL): Promise<CheerioAPI> {
+  try {
+    return (await findHref(
+      atual, 'Matrículas deferidas após o ajuste')
+    )[0]
+  } catch (e) {
+    return (await findHref(
+      (await findHref(anteriores, ' Quadrimestre de '))[1],
+      'Matrículas deferidas após o ajuste'
+    ))[0]
+  }
 }
