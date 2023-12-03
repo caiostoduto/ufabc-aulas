3c8b2555ec929efe1bbe1a91a66f1a1c9313d0d2
Caio Stoduto <caio.stoduto@outlook.com>
2023-12-03 02:04:45 -0300

    chore: restructuring CheerioAPI fetch

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -6,11 +6,12 @@ declare class UFABCAulas {
     ANTERIORES_URL: URL;
     calendarioPdfURL: URL | undefined;
     matriculasPdfURL: URL | undefined;
-    turmasAtualURL: URL | undefined;
+    $matriculas: CheerioAPI | undefined;
     quadris: quadri[] | undefined;
     matriculas: Map<string, string[]> | undefined;
     fetch(): Promise<UFABCAulas>;
     fetchQuadris(): Promise<UFABCAulas>;
-    fetchMatriculas($?: CheerioAPI): Promise<UFABCAulas>;
+    fetchMatriculas(): Promise<UFABCAulas>;
+    private fetchHrefMatriculas;
 }
 export default UFABCAulas;
--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -3,5 +3,4 @@ import { type DocumentInitParameters } from 'pdfjs-dist/types/src/display/api';
 import { type CheerioAPI } from 'cheerio';
 export declare function fetch(url: string, config?: AxiosRequestConfig): Promise<any>;
 export declare function extractTextsFromPdf(docParams: DocumentInitParameters): Promise<string[][]>;
-export declare function findHref(url: URL, contains: string): Promise<[CheerioAPI, URL]>;
-export declare function fetch$Matriculas(atual: URL, anteriores: URL): Promise<CheerioAPI>;
+export declare function findHref(url: URL, contains: string, $?: CheerioAPI): Promise<[CheerioAPI, URL]>;
